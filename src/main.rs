e039ac2c8230ed8b3540b99dcc6e84be58b0e547
Oliver Ocean <github@oliverocean.co>
2020-07-10 16:33:53 -0500

    added 'clap' crate dependency

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
-// main.rs
 
-use std::env;
-use std::fs;
+// STEPS:
+// 1. parse args for options (research and implement clap external lib/crate)
+// 2. read file (use std::fs) 
+// 3. extract from file per options
+// 4. display results
+
+extern crate clap;
+//use clap::{Arg, App, SubCommand};
+use clap::clap_app; // clap_app macro
+
+// to do: use macro or clap lib directly?
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let matches = clap_app!(CrabbyPig =>
+        (version:"v0.1.0")
+        (about: "[ a Rust language Password Idea Generator ]")
+        (@arg minimum: -m --min +takes_value "Minimum word length")
+        (@arg maximum: -M --max +takes_value "Maximum word length")
+        (@arg word_count: -w --wc +takes_value "Total number of words")
+        (@arg dicionary: -d --dict +takes_value "Dicionary source file")
+        (@arg verbose: -v --verbose "verbose mode")
+    )
+    .get_matches();
 
-    let query = &args[1];
-    let filename = &args[2];
-
-    println!("\n> Searching for [ {} ] in file [ {} ]", query, filename);
-
-    let contents = fs::read_to_string(filename)
-        .expect("> !! Can't read that file !!");
-
-    println!("\n---[ Contents of {} ]---\n", filename);
-    println!("{}", contents);
-    println!("\n---[ EOF ]---\n");
+    if matches.is_present("lists") {
+        println!("matches present.");
+    }
 }
