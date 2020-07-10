02d6d424ad7ab45db1bd2f30657fa0e6845f79ad
Oliver Ocean <github@oliverocean.co>
2020-07-10 14:48:48 -0500

    added basic logic to read external files

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 // main.rs
 
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("\n> Searching for [ {} ] in file [ {} ]", query, filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("> !! Can't read that file !!");
+
+    println!("\n---[ Contents of {} ]---\n", filename);
+    println!("{}", contents);
+    println!("\n---[ EOF ]---\n");
 }
